d5f46444ac0de5f23cd02ab3f0ece6eef1356d27
Euler-lne <319512122@qq.com>
2025-09-16 23:37:03 +0800

    暂存 堆

--- Algorithm Competition/1 Base Data Structure/Exercises/5 Heap/P2085.cc
+++ Algorithm Competition/1 Base Data Structure/Exercises/5 Heap/P2085.cc
@@ -12,11 +12,36 @@ int function(vector<int> a, int x)
 int main()
 {
     ios::sync_with_stdio(false);
-    int n;
-    cin >> n;
+    int n, m, max_val = -1;
+    cin >> n >> m;
+    priority_queue<int, vector<int>, greater<int>> min_heap;
+    priority_queue<int> max_heap;
     vector<vector<int>> parameter(n, vector<int>(3, 0));
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < 3; j++) {
+            cin >> parameter[i][j];
         }
+        for (int j = 1; j <= m; j++) {
+            int ans = function(parameter[i], j);
+            if (min_heap.size() < m) {
+                min_heap.push(ans);
+                max_heap.push(ans);
+            } else {
+                if (ans < max_heap.top()) {
+                    max_heap.pop();
+                    max_heap.push(ans);
+                    min_heap.push(ans);
+                }
+            }
+        }
+        // FIXME:内存大小超出限制，一个堆暴力
+        // FIXME:两个堆，大根限制存储范围，超时
     }
+    for (int i = 0; i < m; i++) {
+        cout << min_heap.top();
+        min_heap.pop();
+        if (i != m - 1)
+            cout << " ";
+    }
+    return 0;
 }
