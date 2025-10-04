3702edeed3da5c501f1e9a74018857da0812f40a
Euler-lne <319512122@qq.com>
2025-10-04 15:57:25 +0800

    进击的奶牛

--- Algorithm Competition/2 Base Algorithm/3 Bisection method/P1824.cc
+++ Algorithm Competition/2 Base Algorithm/3 Bisection method/P1824.cc
@@ -11,6 +11,20 @@ const int INF = 0x7fffffff;
 int n, m;
 vector<int> x;
 
+bool check(int mid)
+{
+    int ans = 1;
+    for (int i = 1, pre = 0; i < n; i++) {
+        if (x[i] - x[pre] < mid)
+            continue;
+        else {
+            ans++;
+            pre = i;
+        }
+    }
+    return ans >= m;
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -22,5 +36,14 @@ int main()
         x.push_back(t);
     }
     sort(x.begin(), x.end(), [](int a, int b) { return a < b; });
+    int l = 1, r = x[n - 1] - x[0], mid;
+    while (l < r) {
+        mid = (l + r + 1) >> 1;
+        if (check(mid))
+            l = mid;
+        else
+            r = mid - 1;
+    }
+    cout << l;
     return 0;
 }
