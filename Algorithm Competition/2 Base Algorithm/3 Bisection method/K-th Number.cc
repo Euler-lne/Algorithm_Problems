3aa47c779c0da4533851922e306624faa4c5f0cf
Euler-lne <319512122@qq.com>
2025-10-07 12:51:51 +0800

    添加成绩

--- Algorithm Competition/2 Base Algorithm/3 Bisection method/K-th Number.cc
+++ Algorithm Competition/2 Base Algorithm/3 Bisection method/K-th Number.cc
@@ -1,50 +1,39 @@
-// 二分+尺取法
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-vector<int> a;
-int n, m, k;
-const int MAX = 0x7fffffff;
-
+#define int long long
+const int N = 100010;
+int n, k, m;
+int a[N], s[N];
 bool check(int mid)
 {
-    vector<int> s(n + 1, 0);
-    long long ans = 0;
+    memset(s, 0, sizeof s);
+    int sum = 0, l = 0;
     for (int i = 1; i <= n; i++)
-        s[i] = s[i - 1] + (a[i - 1] >= mid); // 统计大于预计值的前缀和
-    for (int i = k, j = 0; i <= n; i++) {
-        while (s[i] - s[j] >= k) // 这样计算是正确的，区间长度最小为k
-            j++;
-        ans += j;
+        s[i] = s[i - 1] + (a[i] >= mid);
+    for (int i = k; i <= n; i++) {
+        while (s[i] - s[l] >= k)
+            l++;
+        sum += l;
     }
-    return ans >= m;
+    return sum >= m;
 }
-
-int main()
+signed main()
 {
-    ios::sync_with_stdio(false);
-    int t;
-    cin >> t;
-    while (t--) {
-        int l = 1, r = -1, mid;
-        cin >> n >> k >> m;
-        a.resize(n, 0);
-        int min_val = MAX;
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
-            r = max(r, a[i]);
-        }
+    int T;
+    cin >> T;
+
+    while (T--) {
+        scanf("%lld%lld%lld", &n, &k, &m);
+        int l = 0, r = 0;
+        for (int i = 1; i <= n; i++)
+            scanf("%lld", &a[i]), r = max(a[i], r);
         while (l < r) {
-            mid = (l + r + 1) >> 1;
+            int mid = l + r + 1 >> 1;
             if (check(mid))
                 l = mid;
             else
                 r = mid - 1;
         }
-        cout << l << endl;
+        printf("%lld\n", r);
     }
-
-    return 0;
 }
