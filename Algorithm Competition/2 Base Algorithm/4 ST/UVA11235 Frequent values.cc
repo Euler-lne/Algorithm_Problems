4a811241547baa8e39594f77564a5a1639f50c50
Euler_lne <3149512122@qq.com>
2026-02-13 22:12:25 +0800

    简单倍增over

--- Algorithm Competition/2 Base Algorithm/4 ST/UVA11235 Frequent values.cc
+++ Algorithm Competition/2 Base Algorithm/4 ST/UVA11235 Frequent values.cc
@@ -1,67 +1,101 @@
 #include <iostream>
-#include <map>
 using namespace std;
 
-const int N = 1e5 + 2;
-int n, q, a[N], bucket[N], value[N];
-int stmax[N][20], LOG2[N];
-unordered_map<int, int> bucket_map;
+const int N = 1e5 + 3;
 
+int st[N][18];
+int LOG2[N];
+int n, m;
+
+typedef struct
+{
+    int value;
+    int bucket_index;
+} Array;
+Array a[N];
+typedef struct
+{
+    int value, left, right;
+} Bucket;
+Bucket bucket[N];
+
+void get_input();
 void init();
 
 int main()
 {
-    cin >> n >> q;
-    for (int i = 1; i <= n; i++)
-        cin >> a[i];
+    get_input();
     init();
-    while (q--) {
-        int l, r;
-        cin >> l >> r;
-        int l_key = bucket_map[l], r_key = bucket_map[r];
-        int l_count = 1, r_count = 1;
-        for (int i = l; a[i] == a[i + l_count] && i + l_count <= n; l_count++) {
-        }
-        for (int i = r; a[i] == a[i - r_count] && i - r_count >= 0; r_count++) {
-        }
-
-        int end = l + l_count, start = r - r_count;
-        if (start < 0) { // 全部数一样
-            cout << a[1] << endl;
-        } else if (start > end) { // 只有两种数字
-            count << l_count > r_count ? a[l] : a[r] << endl;
+    while (m--) {
+        int left, right;
+        cin >> left >> right;
+        int b_left = a[left].bucket_index;
+        int b_right = a[right].bucket_index;
+        if (b_left == b_right) {
+            cout << b_right - b_left + 1 << endl;
         } else {
-            int max1 = max(l_count, r_count);
-            int val1 = l_count > r_count ? a[l] : a[r];
-            start = bucket_map[start], end = bucket_map[end];
+            int l_count, r_count;
+            int lbr = bucket[b_left].right;
+            int rbl = bucket[b_right].left;
+            l_count = lbr - left + 1;
+            r_count = right - rbl + 1;
+            int len = b_right - b_left - 1;
+            if (len == 0)
+                cout << max(l_count, r_count) << endl;
+            else {
+                int start = b_left + 1, end = b_right - 1;
+                int k = LOG2[len];
+                int mid_count = max(st[start][k], st[end + 1 - (1 << k) + 1][k]);
+                cout << max(l_count, max(mid_count, r_count)) << endl;
+            }
         }
     }
+    cin >> m;
     return 0;
+}
+
+void get_input()
+{
+    cin >> n >> m;
+    int front = -1, count = 0;
+    for (int i = 1, j = 1; i <= n; i++) {
+        cin >> a[i].value;
+        if (i == 1)
+            front = a[i].value;
+        if (front == a[i].value)
+            count++;
+        else {
+            front = a[i].value;
+            bucket[j].value = count;
+            bucket[j].left = i - count;
+            bucket[j].right = i - 1;
+            count = 1;
+            j++;
+        }
+        if (i == n) {
+            bucket[0].value = j;
+            bucket[j].value = count;
+            bucket[j].left = i - count + 1;
+            bucket[j].right = i;
+        }
+    }
 }
 
 void init()
 {
-    for (int i = 1, j = 0; i <= n; j++) {
-        int count = 0;
-        while (i + count <= n && a[i] == a[i + count])
-            count++;
-        bucket[j] = count;
-        value[j] = a[i];
-        bucket_map.insert(a[i], j);
-        bucket[0] = j;
-        i += count;
-    }
     LOG2[0] = -1;
     for (int i = 1; i <= n; i++)
         LOG2[i] = LOG2[i >> 1] + 1;
-
-    for (int i = 0; i <= bucket[0]; i++) {
-        stmax[i][0] = bucket[i];
-    }
-    for (int j = 1; j <= LOG2[bucket[0]]; j++) {
-        for (int i = 0; i <= bucket[0]; i++) {
-            if (i + (1 << (j - 1)) <= bucket[0])
-                stmax = max(stmax[i][j - 1], stmax[i + (1 << (j - 1))][j - 1]);
+    int len = bucket[0].value;
+    for (int i = 1; i <= len; i++)
+        st[i][0] = bucket[i].value;
+    for (int j = 1; j <= LOG2[len]; j++) {
+        for (int i = 1; i + (1 << (j - 1)) <= len; i++) {
+            st[i][j] = max(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
         }
+    }
+    for (int i = 1; i <= len; i++) {
+        for (int j = bucket[i].left; j <= bucket[i].right; j++)
+            a[j].bucket_index = i;
     }
 }
